6bce3fdfd2270bb28a7ebb72ae6a5cbd22e82cee
tinhph89 <tinhpham.fscdn@gmail.com>
2022-03-01 22:59:57 +0700

    commit

--- iwsu/views.py
+++ iwsu/views.py
@@ -4,10 +4,15 @@ from django.shortcuts import render, redirect
 import urllib.request, urllib.parse, urllib.error
 import json
 import ssl
+from diadiem.models import DiaDiem
 def home(request):
-    
-    return render(request, 'home.html')
-    # return HttpResponse('Homepages')
+    danh_sach_dia_diem = DiaDiem.objects.all()
+
+
+    context = {
+        'danh_sach_dia_diem': danh_sach_dia_diem
+    }
+    return render(request, 'home.html', context)
 
 def detail(request):
     return render(request, 'detail.html')
